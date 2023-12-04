12017bee9cc564434b3ad828f9fccfc57572a130
Jean Lucas De Camillo <jean.camilo@crmall.com.br>
2023-12-04 10:45:01 -0300

    fix: dart lints

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ewertonrp/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/ewertonrp/workspace/crmall/flutter-spinning-wheel"
+export "FLUTTER_ROOT=/Users/jeanlucasdecamillo/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/jeanlucasdecamillo/dev/crmall/mobile/libs/flutter-spinning-wheel"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
